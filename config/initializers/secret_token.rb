c93a9665e8e5c83c482bedb9b8e97830e2fb8de9
Juan Figuera <juanfiguera@gmail.com>
2012-12-08 21:10:48 -0500

    Fresh Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DerivativeD::Application.config.secret_token = '4db2179ddb31808c49368d852f83dc66595c7df51d1a8d69e37c794b5712ddef18b2ddf104871ba55c0ae109f04de8ee34eab3cf88fedbb2025a675e81cdf9c8'
